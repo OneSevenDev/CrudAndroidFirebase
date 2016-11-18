49186a307e1dcbce5a3182e3e6629cbd412317f6
manuelguarniz <cruzemg95@gmail.com>
2016-11-18 11:44:02 -0500

    Cambios OK, listado en tiempo real, permite insertar nuevos usuarios. Las modificaciones fueron realizadas en el casteo de los valores antes de ingresarlos al adaptador

--- app/src/main/java/com/begincodes/app/crudandroidfirebase/MainActivity.java
+++ app/src/main/java/com/begincodes/app/crudandroidfirebase/MainActivity.java
@@ -52,6 +52,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //Referencia a la base de datos en firebase
         database = FirebaseDatabase.getInstance();
 
+        //Inicializando variables
         name = (TextView) findViewById(R.id.name);
         nombreEditText = (EditText) findViewById(R.id.nombreEditText);
         apellidosEditText = (EditText) findViewById(R.id.apellidosEditText);
@@ -59,37 +60,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         //Creacion del RecycleView y su adaptador
         recyclerView = (RecyclerView) findViewById(R.id.listPerson);
+        // instancia del Adapter para la lista
         recycleAdapter = new UserRecycleAdapter(this);
+        //agregando el adapter al recycleView (parecido a ListView solo que en MaterialDesing)
         recyclerView.setAdapter(recycleAdapter);
         recyclerView.setHasFixedSize(true);
+        //Condiguro la estricutra de que sea a una fila                    (---)
         final GridLayoutManager layoutManager = new GridLayoutManager(this, 1);
+        //agrego la fila a la lista
         recyclerView.setLayoutManager(layoutManager);
-
-        getData();
 
         guardarButton = (Button) findViewById(R.id.guardarButton);
         guardarButton.setOnClickListener(this);
     }
 
     private void getData() {
-        //Solo estoy haciendo el llamado a un solo dato, para ver si funciona o no
+        //llamo a una lista de datos Json usando noSQL
         mRoot = database.getReference("users");
+        //Indico exactamente el dato que quiero mostar, hasta aqui funciona normal
         mRoot.child("-KWij7oYbhYKVKtX5KfZ").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 try {
                     ArrayList<User> listUser = new ArrayList<>();
+                    //Recojo mi entidad de datos en User, funciona normal
                     User user = dataSnapshot.getValue(User.class);
+                    //Compruebo si mi lista es vacia
                     if (user != null) {
+                        //Esta llena, si llena los daots
                         listUser.add(user);
                     } else {
+                        //En el caso de que este vacio, le puse datos falsos
                         User u = new User();
                         u.setNombre("Error");
                         u.setApellidos("Error");
                         u.setEdad(00);
+                        //agrego datos a la lista
                         listUser.add(u);
                     }
+
+                    //Agrego datos a li adapter
                     recycleAdapter.addItemUser(listUser);
+                    //Por si las dudas imprimimo en el "Monitor" para ver si aun conserva los datos, si conserva datos
                     Log.i(TAG, "Put data list -> " + recycleAdapter.getItemCount());
 
                 } catch (Exception ex) {
@@ -124,7 +136,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onStart() {
         super.onStart();
-        //mRoot = database.getReference("users");
+        getData();
+        getUsersIDs();
+    }
+
+    private void getUsersIDs() {
+        mRoot = database.getReference("users");
+        mRoot.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                try {
+
+                } catch (Exception ex) {
+                    Log.e(TAG, "Error: " + ex.getMessage());
+                    throw ex;
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w(TAG, "Cancelado: ", databaseError.toException());
+                Toast.makeText(getApplicationContext(), R.string.cancelError, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void guardarDatos(String nombre, String apellidos, String edad) {
--- app/src/main/java/com/begincodes/app/crudandroidfirebase/adapters/UserRecycleAdapter.java
+++ app/src/main/java/com/begincodes/app/crudandroidfirebase/adapters/UserRecycleAdapter.java
@@ -17,9 +17,12 @@ import java.util.ArrayList;
  */
 public class UserRecycleAdapter extends RecyclerView.Adapter<UserRecycleAdapter.ViewHolder> {
 
+    //Variables para el adaptador, context (Activity en el que se usara) y la lista
     private Context context;
+    //X cierto si lo igualo a null es lo mismo
     private ArrayList<User> listUser;
 
+    //Contructor
     public UserRecycleAdapter(Context context) {
         this.context = context;
         this.listUser = new ArrayList<>();
@@ -27,28 +30,40 @@ public class UserRecycleAdapter extends RecyclerView.Adapter<UserRecycleAdapter.
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //Infla la vista con el XML que eh diseñado, solo tiene TextView
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.listuserview_item_row, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
+        //Llena los datos al XML, segun los 3 TextView que llene que son nombre apellidos y edad
+        //Todo de acuerdo a los datos que este ingresando mediante el constructor y su posicion
         User user = listUser.get(position);
-        holder.nombre.setText(user.getNombre());
-        holder.apellidos.setText(user.getApellidos());
-        holder.edad.setText(user.getEdad());
-    }
+        holder.nombre.setText(String.valueOf(user.getNombre()));
+        holder.apellidos.setText(String.valueOf(user.getApellidos()));
+        holder.edad.setText(String.valueOf(user.getEdad()));
+// lo puedes castear o parsear a string - ya esta
+   }
 
     @Override
     public int getItemCount() {
+        //Si es que llego a necesitar la cantidad de datos, simplemente se llama esta funcion
         return listUser.size();
     }
 
+    //Mediante este metodo lleno la lista de datos al adaptador
     public void addItemUser(ArrayList<User> list) {
+        if ( listUser != null){
+            listUser.clear();
+        }
         listUser.addAll(list);
+        //Mediante esta linea indico al adaptor que ya tiene datos y los muestre cuando haya cambios
+        //Si esta linea la comento, la APP carga normal xq no llena los datos al RecycleView o Lista que es lo mismo
         notifyDataSetChanged();
     }
 
+    //Clase que es de la vista de datos, esta es la parde defino como sera el detallado de la lista
     public class ViewHolder extends RecyclerView.ViewHolder{
         private TextView nombre;
         private TextView apellidos;
@@ -56,10 +71,10 @@ public class UserRecycleAdapter extends RecyclerView.Adapter<UserRecycleAdapter.
 
         public ViewHolder(View itemView) {
             super(itemView);
+            //Indico a cada variable con que Control estara trabajando (Inicializando Controles o Labels que es lo mismo)
             nombre = (TextView) itemView.findViewById(R.id.nombreTextView);
             apellidos = (TextView) itemView.findViewById(R.id.apellidosTextView);
             edad = (TextView) itemView.findViewById(R.id.edadTextView);
-
         }
     }
 }
